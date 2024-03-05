0a4d5c49b2a5e4cf0ed346258f1975e664f92bef
M01010100 <townsemc@sunypoly.edu>
2024-03-05 15:17:33 -0500

    3/5 git ignore

---
+++ assign04/src/main/java/edu/townsemc/assign04/Box.java
@@ -0,0 +1,14 @@
+package edu.townsemc.assign04;
+
+public class Box {
+    public Box(int sr,int sc,int er,int ec,boolean filled,char drawChar){
+
+    }
+    public String toString(){
+        String s = "";
+        return s;
+    }
+    public void draw(GameBoard s){
+
+    }
+}
---
+++ assign04/src/main/java/edu/townsemc/assign04/GameBoard.java
@@ -0,0 +1,33 @@
+package edu.townsemc.assign04;
+
+public class GameBoard {
+    public GameBoard(int rowCnt, int colCnt, char fillChar){
+
+    }
+    public void clear(){
+
+    }
+    public int getRowCnt(){
+        return 0;
+    }
+    public int getColCnt(){
+        return 0;
+    }
+    public boolean isValidPosition(int row, int col){
+        return false;
+    }
+    public char getPos(int row,int col){
+        return 0;
+    }
+    public boolean setPos(int row, int col, char c){
+        return false;
+    }
+    public String toString(){
+        String s = "";
+        return s;
+    }
+    public String getBoardString(){
+        String s = "";
+        return s;
+    }
+}
